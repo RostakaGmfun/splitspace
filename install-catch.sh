4e958f761ec507e8a9f479bab9a3eaca2c218d75
Rostyslav <rostawesomegd@gmail.com>
2016-02-14 13:20:35 +0200

    Add install-json.sh and install-catch.sh

---
+++ install-catch.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+
+mkdir -p include/catch
+wget https://raw.githubusercontent.com/philsquared/Catch/master/single_include/catch.hpp -O include/catch/catch.hpp
+
---
+++ install-json.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+
+mkdir -p include/json
+wget https://github.com/nlohmann/json/releases/download/v1.1.0/json.hpp -O include/json/json.hpp
+
