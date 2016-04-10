460070aa9f7b3d0aa4611c864cfe3e40753fbc04
Rostyslav <rostawesomegd@gmail.com>
2016-04-10 22:53:31 +0300

    Add RUN_TESTS variable to cmake

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 
-mkdir -p build && cd build && cmake .. && make splitspace -j4 && make -j4 && cd ..
+mkdir -p build && cd build && cmake -DRUN_TESTS=true .. && make splitspace -j4 && make -j4 && cd ..
 ./build/test/splitspace-test
 
